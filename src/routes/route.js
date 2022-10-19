6230b2e50e87e96cf11fbc7440069c4a0f6aa39b
ShashiS07 <shashiss0708@gmail.com>
2022-10-20 00:53:46 +0530

    assignment done

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,73 +1,71 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/students/:name', function(req, res) {
-    let studentName = req.params.name
-    console.log(studentName)
-    res.send(studentName)
-})
-
-router.get("/random" , function(req, res) {
-    res.send("hi there")
-})
-
-
-router.get("/test-api" , function(req, res) {
-    res.send("hi FunctionUp")
-})
-
-
-router.get("/test-api-2" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API")
-})
-
-
-router.get("/test-api-3" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's ")
-})
-
-
-router.get("/test-api-4" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-
-
-router.get("/test-api-5" , function(req, res) {
-    res.send("hi FunctionUp5. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-router.get("/test-api-6" , function(req, res) {
-    res.send({a:56, b: 45})
-})
-
-router.post("/test-post", function(req, res) {
-    res.send([ 23, 45 , 6])
+let players = [
+    {
+        name:"manish",
+        dob:"1/1/1995",
+        gender:"male",
+        city:"jalandhar",
+        sports:["swimming"]
+    },
+    {
+        name:"lokesh",
+        dob:"7/3/1998",
+        gender:"male",
+        city:"mumbai",
+        sports:["football"]
+    },
+    {
+        name:"rohit",
+        dob:"8/9/1987",
+        gender:"male",
+        city:"delhi",
+        sports:["cricket"]
+    }
+]
+router.post("/players", function(req,res){
+    let newplayer=req.body.newplayer1
+    let repeatedname=false;
+    for(let i=0;i<players.length;i++){
+        if(players[i].name===newplayer.name){
+            repeatedname=true;
+        }
+    }
+    if(repeatedname){
+        res.send({error:"already exist"})
+    }else{
+        players.push(newplayer)
+        res.send(players)
+    }
 })
 
 
-router.post("/test-post-2", function(req, res) {
-    res.send(  { msg: "hi" , status: true }  )
-})
-
-router.post("/test-post-3", function(req, res) {
-    // let id = req.body.user
-    // let pwd= req.body.password
-
-    // console.log( id , pwd)
-
-    console.log( req.body )
-
-    res.send(  { msg: "hi" , status: true }  )
+router.get("/sol1",function(req,res){
+    let arr=[1,2,3,5,6,7];
+    let n=arr[arr.length-1]
+    let total=n*(n+1)/2
+    let sum=0;
+    for(let i=0; i<arr.length;i++){
+        sum=sum+arr[i];
+    }
+    let missing=(total-sum);
+    res.send({missingNumber:missing})
 })
 
 
-
-router.post("/test-post-4", function(req, res) {
-    let arr= [ 12, "functionup"]
-    let ele= req.body.element
-    arr.push(ele)
-    res.send(  { msg: arr , status: true }  )
+router.get("/sol2", function(req,res){
+    let arr=[33,34,35,37,38,]
+    let N=arr.length+1
+    let first=arr[0]
+    let last=arr[arr.length-1]
+    let total=N*(first+last)/2
+    let sum=0
+    for(let i=0;i<arr.length;i++){
+        sum=sum+arr[i]
+    }
+    let missingNo=total-sum;
+    res.send({missingNumber:missingNo})
 })
-
+  
 module.exports = router;
