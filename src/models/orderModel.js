d0b3987aca1380e782b10aace5a83c3c197a8632
ShashiS07 <shashiss0708@gmail.com>
2022-11-03 00:26:28 +0530

    new file add

---
+++ src/models/orderModel.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const ObjectId=mongoose.Schema.Types.ObjectId
+const orderSchema = new mongoose.Schema( {
+    userId:ObjectId,
+    productId:ObjectId,
+    amount:Number,
+    isFreeUse:Boolean,
+    date:Date,
+}, { timestamps: true });
+
+
+module.exports = mongoose.model('Order', orderSchema) //users
