90cdea152d0f8068daced4cdec71f482dc55d8b9
ShashiS07 <shashiss0708@gmail.com>
2022-11-07 20:23:51 +0530

    assignment done with the help oftry and catch

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,53 +2,77 @@ const jwt = require("jsonwebtoken");
 const userModel = require("../models/userModel");
 
 const createUser = async function (req, res) {
-
+try{
   let data=req.body;
-  let savedData= await userModel.create(data);
-  res.send({users:savedData})
+  if(Object.keys(data).length!=0){
+    let savedData= await userModel.create(data);
+    res.status(201).send({users:savedData})
+  }else{
+    res.status(400).send({msg:"Bad Request"})
+  }
+}
+catch(error){
+  res.status(500).send({msg:"error", error: error.message})
+}
 };
 
 const loginUser = async function (req, res) {
-
+try{
   let username=req.body.emailId
   let password=req.body.password
 
   let user=await userModel.findOne({emailId:username,password:password})
-  if(!user) return res.send({status:false, error:"Username or password is invalid"});
+  if(!user) return res.status(400).send({status:false, error:"Username or password is invalid"});
+
   let token= jwt.sign({userId:user._id.toString()},"assignment-key")
   res.setHeader("x-auth-token",token);
-  res.send({status:true,token:token})
-
+  res.status(201).send({status:true,token:token})
+}
+catch(error){
+  res.status(500).send({msg:"error",error:error.message})
+}
 };
 
 const getUserData = async function (req, res) {
-
+  try{
     let userId= req.params.userId;
     let userdetail= await userModel.findById(userId)
-    
-    res.send({status:true,user:userdetail})
 
+    // if(!userdetail) res.status(400).send({status:false, error:"No such user exist"})
+    
+    res.status(200).send({status:true,user:userdetail})
+  }
+  catch(error){
+    res.status(500).send({error:error.message})
+  }
 };
 
 const updateUser = async function (req, res) {
-
+try{
   let userId = req.params.userId;
-  let user = await userModel.findById(userId);
+  // let user = await userModel.findById(userId);
  
   let userData = req.body;
   let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, {$set:userData},{new:true});
-  res.send({ status: true, data: updatedUser });
+  res.status(200).send({ status: true, data: updatedUser });
+}
+catch(error){
+  res.status(500).send({error:error.message})
+}
 };
 
 const deleteuser= async function(req,res){
-
+try{
   let userId = req.params.userId;
-  let user = await userModel.findById(userId);
+  // let user = await userModel.findById(userId);
 
   let userData = req.body;
   let deleteUser = await userModel.findOneAndUpdate({ _id: userId }, {$set:userData},{new:true});
-  res.send({ status: true, data: deleteUser });
-
+  res.status(201).send({ status: true, data: deleteUser });
+}
+catch(error){
+  res.status(500).send({error:error.message})
+}
 }
 
 module.exports.createUser = createUser;
--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -2,24 +2,29 @@ const jwt=require('jsonwebtoken')
 const userModel=require("../models/userModel")
 
 const middleware=async function(req,res,next){
+  try{
     let token =req.headers["x-Auth-token"]
     if (!token) token = req.headers["x-auth-token"];
-    if(!token) return res.send({status:false, error:"token must be present"})
+    if(!token) return res.status(404).send({status:false, error:"token must be present"})
   
-    let decodetoken=await jwt.verify(token,"assignment-key")
+    let decodetoken= jwt.verify(token,"assignment-key")
   
-    if(!decodetoken) return res.send({status:false,error:"token is invalid"})
+    // if(!decodetoken) return res.status(403).send({status:false,error:"token is invalid"})
   
     let userId = req.params.userId;
     let user = await userModel.findById(userId);
    
     if (!user) {
-      return res.send({status:false,error:"No such user exist"});
+      return res.status(403).send({status:false,error:"No such user exist"});
     }
     if(decodetoken.userId !==userId){
-      return res.send({status:false,error:"user are not authorised"})
+      return res.status(401).send({status:false,error:"user are not authorised"})
     }
     next();
+  }
+  catch(error){
+    res.status(500).send({error:error.message})
+  }
 }
 
 module.exports.middleware=middleware
